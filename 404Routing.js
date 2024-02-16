de5563b79885b83eaf6491df43652b5a62ef445b
swdr165 <none@none.com>
2024-02-16 14:40:47 -0800

    ext

--- 404Routing.js
+++ 404Routing.js
@@ -1,4 +1,18 @@
-const pathLabel = document.createElement('h2')
-document.body.appendChild(pathLabel)
+// '/' in src ususaly access root. Ex: 'yourWebSite.com' or 'localhost:port',
+// in case of github pages path to your site looks like this 'yourName.github.io/yourRepo'
+// that meens that root is 'yourName.github.io'
+// and u have to provide your repo name to src like that
+// '<script type="module" src="/yourRepo/404Routing.js"></script>'.
+// Same for routing fetch.
 
-pathLabel.textContent = window.location.pathname
+const curPath = window.location.pathname
+const curPage = window.location.pathname.slice(1)
+
+fetch(`/route/${curPage}.html`)
+    .then( response => {return response.text()})
+    .then( data => {
+        document.body.innerHTML = data
+        const pathLabel = document.createElement('h2')
+        document.body.appendChild(pathLabel)
+        pathLabel.textContent = `Path: ${curPath} | Page: ${curPage}`
+    })
